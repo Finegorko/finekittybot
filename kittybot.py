e9fab6e2765df309e7d97e75c1574d49d566885a
Alexander Boykov <finegorko@gmail.com>
2022-08-13 18:34:24 +0300

    dog added

--- kittybot.py
+++ kittybot.py
@@ -8,55 +8,76 @@ from telegram.ext import CommandHandler, Updater
 
 load_dotenv()
 
-secret_token = os.getenv('TELEGRAM_TOKEN')
+secret_token = os.getenv("TELEGRAM_TOKEN")
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
 
-URL = "https://api.thecatapi.com/v1/images/search"
 
-
-def get_new_image():
+def get_new_cat_image():
+    URL = "https://api.thecatapi.com/v1/images/search"
     try:
         response = requests.get(URL)
     except Exception as error:
-        logging.error(f'Ошибка при запросе к основному API: {error}')     
-        new_url = 'https://api.thedogapi.com/v1/images/search'
+        logging.error(f"Ошибка при запросе к основному API: {error}")
+        new_url = "https://api.thedogapi.com/v1/images/search"
         response = requests.get(new_url)
-    
+
     response = response.json()
-    random_cat = response[0].get('url')
-    return random_cat 
+    random_cat = response[0].get("url")
+    return random_cat
+
+
+def get_new_dog_image():
+    URL = "https://api.thedogapi.com/v1/images/search"
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        logging.error(f"Ошибка при запросе к основному API: {error}")
+        new_url = "https://api.thedocatapi.com/v1/images/search"
+        response = requests.get(new_url)
+
+    response = response.json()
+    random_dog = response[0].get("url")
+    return random_dog
 
 
 def new_cat(update, context):
     chat = update.effective_chat
-    context.bot.send_photo(chat.id, get_new_image())
+    context.bot.send_photo(chat.id, get_new_cat_image())
+
+
+def new_dog(update, context):
+    chat = update.effective_chat
+    context.bot.send_photo(chat.id, get_new_dog_image())
 
 
 def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
-    button = ReplyKeyboardMarkup([["/newcat"]], resize_keyboard=True)
+    button = ReplyKeyboardMarkup(
+        [["/newcat"], ["/newdog"]], resize_keyboard=True
+    )
 
     context.bot.send_message(
         chat_id=chat.id,
-        text=f"Привет, {name}. Посмотри, какого котика я тебе нашёл!",
+        text=f"Привет, {name}. Кого хочешь, чтобы я тебе показал?",
         reply_markup=button,
     )
-    context.bot.send_photo(chat.id, get_new_image())
 
 
 def main():
     updater = Updater(token=secret_token)
 
-    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+    updater.dispatcher.add_handler(CommandHandler("start", wake_up))
+    updater.dispatcher.add_handler(CommandHandler("newcat", new_cat))
+    updater.dispatcher.add_handler(CommandHandler("newdog", new_dog))
 
     updater.start_polling()
     updater.idle()
 
 
-if __name__ == '__main__':
-    main() 
+if __name__ == "__main__":
+    main()
